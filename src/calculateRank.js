446511f465fc10ff2c845352b1185b06767ffd1b
Kaan Karakaya <yusufkaan142@gmail.com>
2021-03-06 19:00:47 +0300

    I should be S+

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -63,29 +63,7 @@ function calculateRank({
 
   const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
 
-  let level = "";
-
-  if (normalizedScore < RANK_S_VALUE) {
-    level = "S+";
-  }
-  if (
-    normalizedScore >= RANK_S_VALUE &&
-    normalizedScore < RANK_DOUBLE_A_VALUE
-  ) {
-    level = "S";
-  }
-  if (
-    normalizedScore >= RANK_DOUBLE_A_VALUE &&
-    normalizedScore < RANK_A2_VALUE
-  ) {
-    level = "A++";
-  }
-  if (normalizedScore >= RANK_A2_VALUE && normalizedScore < RANK_A3_VALUE) {
-    level = "A+";
-  }
-  if (normalizedScore >= RANK_A3_VALUE && normalizedScore < RANK_B_VALUE) {
-    level = "B+";
-  }
+  let level = "S+";
 
   return { level, score: normalizedScore };
 }
